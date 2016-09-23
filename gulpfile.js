024dd7fbcfc3689230e44068fd5e2b3228669b30
vdyshkant <yong.knox@yandex.ru>
2016-09-23 14:42:03 +0300

    finished styles for fitments page:functions, on recently created 3d page

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,8 @@ gulp.task('pug', function() {
     // '!src/templates/conf/*.pug', // ignore this file
     // 'src/templates/**/*.pug'   // ignore all files
     'src/templates/index.pug',
-    'src/templates/windsystem.pug'
+    'src/templates/windsystem.pug',
+    'src/templates/fitments.pug'
   ])     // выборка files from glob
         .pipe(pug( { pretty: true } ))                             // push this^^^ query to pug pluguin
         .on('error', errorLog)
