9ea22d0c970c5000228ad3b7fb94ef6b7ab71afa
vdyshkant <yong.knox@yandex.ru>
2016-11-03 10:49:21 +0200

    :construction: page-fitments(3)::gallery-1 -- made adaptation; beta ver.

--- src/js/main.js
+++ src/js/main.js
@@ -703,35 +703,41 @@ $(document).ready(function() {
 $(document).ready(function() {
 
 	if($('body').is('.page-fitments')){
-		$("#gallery-smooth").smoothDivScroll({
-			mousewheelScrolling: true,
-			manualContinuousScrolling: true,
-			visibleHotSpotBackgrounds: "always",
-			autoScrollingMode: "always"
-		});
+
+    // if (window.matchMedia("(min-width: 768px)").matches) {
+
+  		$("#gallery-smooth").smoothDivScroll({
+  			mousewheelScrolling: true,
+  			manualContinuousScrolling: true,
+  			visibleHotSpotBackgrounds: "always",
+  			autoScrollingMode: "always"
+  		});
 
 
-		// This is just to make the scroller pause...
-			// Mouse over
-			$("#gallery-smooth").bind("mouseover", function(){
-					$("#gallery-smooth").smoothDivScroll("stopAutoScrolling");
-			});
+  		// This is just to make the scroller pause...
+  			// Mouse over
+  			$("#gallery-smooth").bind("mouseover", function(){
+  					$("#gallery-smooth").smoothDivScroll("stopAutoScrolling");
+  			});
 
-			// Mouse out
-			$("#gallery-smooth").bind("mouseout", function(){
-					$("#gallery-smooth").smoothDivScroll("startAutoScrolling");
-			});
+  			// Mouse out
+  			$("#gallery-smooth").bind("mouseout", function(){
+  					$("#gallery-smooth").smoothDivScroll("startAutoScrolling");
+  			});
 
-		// fancy box for this gallery
-		$("#gallery-smooth a.gallery__full").fancybox({
-			// Prevent fancybox from causing page to to jump to the top
-			helpers: {
-				overlay: {
-					locked: false
-				}
-			}
-		});
-		// fancy box for this gallery END
+  		// fancy box for this gallery
+  		$("#gallery-smooth a.gallery__full").fancybox({
+  			// Prevent fancybox from causing page to to jump to the top
+  			helpers: {
+  				overlay: {
+  					locked: false
+  				}
+  			}
+  		});
+  		// fancy box for this gallery END
+
+    // }
+
 	}
 
 });
