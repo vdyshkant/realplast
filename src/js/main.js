02292617539be250ad7dd37671c817b66eca4e67
vdyshkant <yong.knox@yandex.ru>
2016-12-14 10:37:08 +0300

    index::feedbacks -- adaptation && fixes

--- src/js/main.js
+++ src/js/main.js
@@ -168,10 +168,29 @@ $(document).ready(function() {
 // tablet ver
 $(document).ready(function() {
 	if($('body').is('.page-index')){
-	  if ((window.matchMedia("(max-width: 1359px)").matches)) {
-
+	  if ((window.matchMedia("(min-width: 768px)").matches) && (window.matchMedia("(max-width: 1359px)").matches)) {
       var carousel = $("#carousel").waterwheelCarousel({
   	    flankingItems: 0,
+        movingToCenter: function($movingFromCenter) {
+
+
+          // $newCenterItem is a jQuery wrapped object describing the image that was clicked.
+          var height = $movingFromCenter.innerHeight();
+          var position = $movingFromCenter.position();
+              height = parseInt(height, 10);
+              position = parseInt(position.top, 10);
+          var num = height + position;
+              num = position + (height / 2 - 140);
+
+          // console.log($movingFromCenter + ' item height: ' + height);
+          // console.log($movingFromCenter + ' item position.top: ' + position);
+          // console.log('sum: ' + num);
+          // setTimeout(function(){
+          $( ".slider-controls" ).css( "top", num);
+          // });
+
+          // Now that we have the ID of the image, we can use jQuery to show the content corresponding to the tigerpicture.
+        },
         // horizon: 10,
         // horizonOffset: -50,
         // horizonOffsetMultiplier: .7
@@ -202,8 +221,49 @@ $(document).ready(function() {
 
       $('.slider-item').css('max-width', '300px');
 
+      // setPositinForControls();
+      // function setPositinForControls() {
+      //   var centerItem = $('.carousel-center');
+      //   if (centerItem) {
+      //     var height = centerItem.innerHeight();
+      //         height = parseInt(height, 10);
+      //
+      //     var position = centerItem.offset();
+      //     var top = parseInt(position.top, 10);
+      //     var newtop = height + top;
+      //         newtop = newtop - 71 + 'px';
+      //
+      //     $('.slider-controls').css( {
+      //         'top': newtop
+      //     });
+      //   }
+      // }
+
+
+
       var carousel = $("#carousel").waterwheelCarousel({
   	    flankingItems: 0,
+        sizeMultiplier: 1,
+        movingToCenter: function($movingFromCenter) {
+
+
+          // $newCenterItem is a jQuery wrapped object describing the image that was clicked.
+          var height = $movingFromCenter.innerHeight();
+          var position = $movingFromCenter.position();
+              height = parseInt(height, 10);
+              position = parseInt(position.top, 10);
+      	  var num = height + position;
+
+          // console.log($movingFromCenter + ' item height: ' + height);
+          // console.log($movingFromCenter + ' item position.top: ' + position);
+          // console.log('sum: ' + num);
+          // setTimeout(function(){
+          $( ".slider-controls" ).css( "top", num - 71);
+          // });
+
+          // Now that we have the ID of the image, we can use jQuery to show the content corresponding to the tigerpicture.
+        },
+
   	    // smallFeatureWidth: 1,
   	    // smallFeatureOffset: 150,
   	    // startingFeature: 3
@@ -212,12 +272,13 @@ $(document).ready(function() {
 
   	  $('#prev').bind('click', function () {
   	    carousel.prev();
-  	    return false;
+        // if we want to stop carousel from carouselling, use return below:
+  	    // return false;
   	  });
 
   	  $('#next').bind('click', function () {
   	    carousel.next();
-  	    return false;
+  	    // return false;
   	  });
 
     } // eof matchMedia
