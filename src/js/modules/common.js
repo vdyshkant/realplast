78da37eb81486bdda97ff7f3fc2e0efe37c13b6f
vdyshkant <yong.knox@yandex.ru>
2016-12-23 14:06:15 +0300

    ::breadcrumbs -- updated "..." shorter module.

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -44,7 +44,7 @@ RESET
 
 
 
-  /* отрабатывать 
+  /* отрабатывать
     ================================================ */
   // (function() {
   //   var a = "initial_template";
@@ -1791,3 +1791,37 @@ $(document).ready(function() {
   // } // eof body.is
 }); // eof ready
 // eof burger
+
+
+
+// breadcrumbs '...'
+if ($(window).width() < 768) {
+		(function($) {
+				// You pass-in jQuery and then alias it with the $-sign
+				// So your internal code doesn't change
+				var count = $('.breadcrumbs li').length;
+				if (count >= 3) {
+						$('.breadcrumbs li:nth-child(n+2):not(:last-child) a').html('...');
+				}
+				$('.breadcrumbs li:last-child a').addClass('overcrumbs');
+				// http://stackoverflow.com/questions/4291151/jquery-count-child-elements
+
+				// counting width for whole vreadcrumbs
+				var parent = $('.breadcrumbs').width();
+				// var first = $('.breadcrumbs li:not(:last-child)').outerWidth(true);
+
+				var w = 0;
+
+				jQuery('.breadcrumbs li:last-child').each(function() {
+						jQuery(this).prevAll().each(function() {
+								w += $(this).outerWidth(true);
+								console.log(w);
+						});
+				});
+				var newWidthForLastEl = parent - w - 10;
+
+				$('.breadcrumbs li:last-child').width(newWidthForLastEl);
+
+		})(jQuery);
+}
+// EOF breadcrumbs '...'
