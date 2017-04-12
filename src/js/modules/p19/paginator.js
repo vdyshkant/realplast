cca6c02efb56e71e230a91a229c8c2a4971668e6
vdyshkant <yong.knox@yandex.ru>
2017-04-12 20:27:33 +0300

    page-video(19) -- made adaptation, beta ver

--- src/js/modules/p19/paginator.js
+++ src/js/modules/p19/paginator.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
 
 
     // basic paging logic to demo the buttons
-    var pr = document.querySelector( '.paginate.left' );
-    var pl = document.querySelector( '.paginate.right' );
+    var pr = document.querySelector( '.objects .paginate.left' );
+    var pl = document.querySelector( '.objects .paginate.right' );
 
     pr.onclick = slide.bind( this, -1 );
     pl.onclick = slide.bind( this, 1 );
@@ -24,7 +24,7 @@ $(document).ready(function() {
   function slide(offset) {
     index = Math.min( Math.max( index + offset, 0 ), total - 1 );
 
-    document.querySelector( '.counter' ).innerHTML = ( index + 1 ) + ' / ' + total;
+    document.querySelector( '.objects .counter' ).innerHTML = ( index + 1 ) + ' / ' + total;
 
     pr.setAttribute( 'data-state', index === 0 ? 'disabled' : '' );
     pl.setAttribute( 'data-state', index === total - 1 ? 'disabled' : '' );
