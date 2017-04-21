364dcdb1ea579757a8bff9cadf329573e5c3912b
vdyshkant <yong.knox@yandex.ru>
2017-04-21 15:33:59 +0300

    page-calc(22) -- finished layout

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -1885,3 +1885,29 @@ $(document).ready(function() {
 
   }		// eof ('body').is
 }); // eof document READY
+
+
+// page-calc :: calc plus-minus module
+$(document).ready(function() {
+	if($('body').is('.page-calc')){
+
+					$('.parameters-form-minus').on('click',function(event) {
+				    event.preventDefault();
+				    current = +$(this).siblings('input').val();
+				    input = $(this).siblings('input');
+				    $(input).val(current - 1);
+
+				    if (current < 1) {
+				      $(input).val(current);
+				    }
+				  });
+
+				  $('.parameters-form-plus').on('click',function(event) {
+				    event.preventDefault();
+				    current = +$(this).siblings('input').val();
+				    input = $(this).siblings('input');
+				    $(input).val(current + 1);
+				  });
+
+  }		// eof ('body').is
+}); // eof document READY
